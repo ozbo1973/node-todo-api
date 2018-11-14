72ade27abaa70b21f414dedc6c46b601e1381e3c
Brady <bbbove20@gmail.com>
2018-11-14 14:28:31 -0700

    adding to heroku.

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost:27017/node-todo-api", {
+mongoose.connect(process.env.MONGODB_URI, {
   keepAlive: true,
   useNewUrlParser: true
 });
